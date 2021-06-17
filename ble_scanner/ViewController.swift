869bf6b61ff847769ee89d60203db6b294d8b4cc
Seth Polyniak <sethpolyniak@sp.fios-router.home>
2021-06-17 16:46:13 -0400

    Added didDiscover func to list peripheral names

--- ble_scanner/ViewController.swift
+++ ble_scanner/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     // Object that scans, discovers, connects, manages, peripherals
     var centralManager: CBCentralManager!
     
-    // Reference to peripheral object when connection is established
-    var peripheral: CBPeripheral!
+    // Reference to chosen peripheral object when connection is established
+    var chosenPeripheral: CBPeripheral!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +27,34 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
 
     // Checks hardware status of Bluetooth on your device (powered on, BLE available/enabled, etc.)
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        // BLE is on, start scanning for peripherals
         if central.state == CBManagerState.poweredOn {
             print("BLE powered on")
+            
+            // Could populate the withServices param to scan for specific UUIDs (like in the bauer lock example)
+            central.scanForPeripherals(withServices: nil, options: nil)
+            
+            
         }
+        // BLE is off or not found, print err message
         else {
             print("ERROR: BLE not compatible or off")
+        }
+    }
+    
+    // Is called for each peripheral that is found in the scanForPeripherals func
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        // Check if peripheral has name, assign it, and print
+        if let name = peripheral.name {
+            print(name)
+            
+            // Stop scanning and connect to peripheral
+            self.centralManager.stopScan()
+            self.chosenPeripheral = peripheral     // Save class-level reference of chosen peripheral
+            self.chosenPeripheral.delegate = self  // Delegate self to the chosenPeripheral
+            
+            self.centralManager.connect(peripheral, options: nil)  // Finally connect
         }
     }
 
