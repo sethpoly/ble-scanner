2e66a1dae2a0fce1a0baa1fea2a5f18177d082d4
Seth Polyniak <sethpolyniak@sp.fios-router.home>
2021-06-17 17:58:31 -0400

    Customized BLETableViewCell, added nav controller for filter function

---
+++ ble_scanner/BLETableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BLETableViewCell.swift
+//  ble_scanner
+//
+//  Created by Seth Polyniak on 6/17/21.
+//  Copyright © 2021 Seth Polyniak. All rights reserved.
+//
+
+import UIKit
+
+class BLETableViewCell: UITableViewCell {
+    
+    
+    
+}
--- ble_scanner/BLETableViewController.swift
+++ ble_scanner/BLETableViewController.swift
@@ -37,7 +37,7 @@ class BLETableViewController: UITableViewController, CBCentralManagerDelegate, C
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bleTableViewCell", for: indexPath)
         
-        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+       // cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
 
         return cell
     }
