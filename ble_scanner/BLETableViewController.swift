b8f5b9c463bbb4561158ac671ab2ea36d15585f5
Seth Polyniak <sethpolyniak@sp.fios-router.home>
2021-06-17 17:20:16 -0400

    Added tableviewcontroller class, and changed entry point

--- ble_scanner/BLETableViewController.swift
+++ ble_scanner/BLETableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BLETableViewController.swift
 //  ble_scanner
 //
 //  Created by Seth Polyniak on 6/17/21.
@@ -7,9 +7,9 @@
 //
 
 import UIKit
-import CoreBluetooth  // Framework used to manage any BLE functionality
+import CoreBluetooth
 
-class ViewController: UITableViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+class BLETableViewController: UITableViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     
     // Object that scans, discovers, connects, manages, peripherals
     var centralManager: CBCentralManager!
@@ -17,14 +17,33 @@ class ViewController: UITableViewController, CBCentralManagerDelegate, CBPeriphe
     // Reference to chosen peripheral object when connection is established
     var chosenPeripheral: CBPeripheral!
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Init centralManager
         centralManager = CBCentralManager(delegate: self, queue: nil)
-        
     }
 
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+    
     // Checks hardware status of Bluetooth on your device (powered on, BLE available/enabled, etc.)
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         // BLE is on, start scanning for peripherals
@@ -33,10 +52,8 @@ class ViewController: UITableViewController, CBCentralManagerDelegate, CBPeriphe
             
             // Could populate the withServices param to scan for specific UUIDs (like in the bauer lock example)
             central.scanForPeripherals(withServices: nil, options: nil)
-            
-            
         }
-        // BLE is off or not found, print err message
+            // BLE is off or not found, print err message
         else {
             print("ERROR: BLE not compatible or off")
         }
@@ -64,4 +81,3 @@ class ViewController: UITableViewController, CBCentralManagerDelegate, CBPeriphe
     }
 
 }
-
